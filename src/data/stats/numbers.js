5b60133f985dc16b4a17efa7e57eaa51a51f56bc
brewksin <sbrukson@gmail.com>
2023-04-30 21:51:31 -0400

    working buildgit add .!

--- src/data/stats/numbers.js
+++ src/data/stats/numbers.js
@@ -2,51 +2,51 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class DateFact extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: null,
-            loading: true,
-            error: null,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      loading: true,
+      error: null,
+    };
+  }
+  // state = {
+  //     data: null,
+  //     loading: true,
+  //     error: null,
+  // };
+
+  async componentDidMount() {
+    try {
+      const now = Date.now();
+      const response = await axios.get(`http://numbersapi.com/${now.getMonth()}/${now.getDate()}}/date`);
+      this.setState({ data: response.data, loading: false });
+    } catch (error) {
+      this.setState({ error, loading: false });
     }
-    // state = {
-    //     data: null,
-    //     loading: true,
-    //     error: null,
-    // };
+  }
 
-    async componentDidMount() {
-        try {
-            const now = Date.now();
-            const response = await axios.get(`http://numbersapi.com/${now.getMonth()}/${now.getDate()}}/date`);
-            this.setState({ data: response.data, loading: false });
-        } catch (error) {
-            this.setState({ error, loading: false });
-        }
+  render() {
+    const { data, loading, error } = this.state;
+
+    if (loading) {
+      return <p>Loading...</p>;
     }
 
-    render() {
-        const { data, loading, error } = this.state;
-
-        if (loading) {
-            return <p>Loading...</p>;
-        }
-
-        if (error) {
-            return <p>Oops, something went wrong: {error.message}</p>;
-        }
-
-        return (
-            <div>
-                {data && (
-                    <ul>
-                        {data}
-                    </ul>
-                )}
-            </div>
-        );
+    if (error) {
+      return <p>Oops, something went wrong: {error.message}</p>;
     }
+
+    return (
+      <div>
+        {data && (
+          <ul>
+            {data}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
 export default DateFact;
